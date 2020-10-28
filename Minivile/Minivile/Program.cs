500dcb9495a7b1b6c99f8d8eea79b021efa05ed6
FaerDerr <matheo.souville@gmail.com>
2020-10-28 11:54:49 +0100

    C'est très le test

--- Minivile/Minivile/Program.cs
+++ Minivile/Minivile/Program.cs
@@ -6,7 +6,7 @@ namespace Minivile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pti miniville là ?");
         }
     }
 }
