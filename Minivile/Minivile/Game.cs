13b7b51267ef0db172369f9cce8636464983db5d
FaerDerr <matheo.souville@gmail.com>
2020-10-31 23:09:23 +0100

    Affichage, choix, corrections de bugs... Dites-le si je vous emmerde mdr

--- Minivile/Minivile/Game.cs
+++ Minivile/Minivile/Game.cs
@@ -11,34 +11,66 @@ namespace Miniville
 
         public Die de = new Die();
         public Pile pile = new Pile();
+        public List<Card> mainJoueur;
+        public List<Card> mainMachine;
 
         public Game()
         {
-            joueurHumain = new Player("humain", 0);
-            joueurMachine = new Player("ia", 0);
-            
-        }
+            mainJoueur = new List<Card>();
+            mainJoueur.Add(new Card(1, "Bleu", 1, "Champs de blé", "Recevez 1 pièce", 1, 1));
+            mainJoueur.Add(new Card(3, "Vert", 1, "Boulangerie", "Recevez 2 pièces", 2, 2));
+            joueurHumain = new Player("humain", 3, mainJoueur);
 
+            mainMachine = new List<Card>();
+            mainMachine.Add(new Card(1, "Bleu", 1, "Champs de blé", "Recevez 1 pièce", 1, 1));
+            mainMachine.Add(new Card(3, "Vert", 1, "Boulangerie", "Recevez 2 pièces", 2, 2));
+            joueurMachine = new Player("ia", 3, mainMachine);
+        }
+            
         public void Run()
         {
-            while (joueurHumain.argent < 20 || joueurMachine.argent < 20) {
+            while (joueurHumain.argent <= 20 || joueurMachine.argent <= 20) {
                 //tour Joueur
+                Console.Clear();
                 Lancer();
                 joueurHumain.lanceur = true;
-                joueurMachine.Regarder(de.face, "vert", joueurHumain);
-                joueurHumain.Regarder(de.face, "rouge", joueurMachine);
+                Tour(joueurHumain);
+                Resultats(joueurHumain);
+
                 joueurHumain.Jouer(pile.pileCards);
                 joueurHumain.lanceur = false;
 
                 //tour IA
                 Lancer();
                 joueurMachine.lanceur = true;
-                joueurHumain.Regarder(de.face, "vert", joueurMachine);
-                joueurMachine.Regarder(de.face, "rouge",joueurHumain);
+                Tour(joueurMachine);
+                Resultats(joueurMachine);
+
                 joueurMachine.Jouer(pile.pileCards);
                 joueurMachine.lanceur = false;
+            }
 
-                //IDEM, MAIS AVEC HUMAIN -> MACHINE
+            Console.Clear();
+
+            if (joueurHumain.argent >= 20 && joueurMachine.argent < 20)
+            {
+                Console.WriteLine("LE JOUEUR {0} GAGNE LA PARTIE AVEC {1} PIECE", joueurHumain.joueur.ToUpper(), joueurHumain.argent);
+            }
+            else if (joueurMachine.argent >= 20 && joueurHumain.argent < 20)
+            {
+                Console.WriteLine("LE JOUEUR {0} GAGNE LA PARTIE AVEC {1} PIECE", joueurMachine.joueur.ToUpper(), joueurMachine.argent);
+            }
+            else if (joueurMachine.argent > joueurHumain.argent)
+            {
+                Console.WriteLine("LE JOUEUR {0} GAGNE LA PARTIE AVEC {1} PIECE", joueurMachine.joueur.ToUpper(), joueurMachine.argent);
+            }
+            else if (joueurHumain.argent > joueurMachine.argent)
+            {
+                Console.WriteLine("LE JOUEUR {0} GAGNE LA PARTIE AVEC {1} PIECE", joueurHumain.joueur.ToUpper(), joueurHumain.argent);
+            }
+            else
+            {
+                Console.WriteLine("LE JEU SE CONCLUE SUR UNE EGALITE !? RT SI C TRIST");
             }
 
         }
@@ -47,6 +79,63 @@ namespace Miniville
         {
             de.Lancer();
             return de.face;
+        }
+
+        public void Resultats(Player player)
+        {
+            Console.Clear();
+            if (player == joueurHumain)
+            {
+                Console.WriteLine("---                     C'est à votre tour !                     ---");
+                Console.WriteLine("---                     LE DE A FAIT UN {0} !                    ---\n", de.face);
+                joueurMachine.Regarder(de.face, "vert", joueurHumain);
+                Console.WriteLine();
+                joueurHumain.Regarder(de.face, "rouge", joueurMachine);
+                Console.WriteLine("\nAppuyer sur entrée pour finir le tour !");
+                
+            }
+            else
+            {
+                Console.WriteLine("---                     C'est le tour de L'IA !                     ---");
+                Console.WriteLine("---                     LE DE A FAIT UN {0} !                       ---\n", de.face);
+                joueurMachine.Regarder(de.face, "vert", joueurMachine);
+                Console.WriteLine();
+                joueurHumain.Regarder(de.face, "rouge", joueurHumain);
+                Console.WriteLine("\nAppuyer sur entrée pour finir le tour !");
+                
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public void Tour(Player player) 
+        {
+            Console.Clear();
+            if (player == joueurHumain)
+            {
+                Console.WriteLine("---                     C'est à votre tour !                     ---\n");
+                Console.WriteLine("> Votre argent : {0}                       - L'Argent de l'IA : {1}\n", joueurHumain.argent, joueurMachine.argent);
+                Console.WriteLine("> Vos cartes actives :");
+                foreach(Card c in joueurHumain.main)
+                {
+                    Console.WriteLine(" - " + c.ToString());
+                }
+                Console.WriteLine("\n\nAppuyer sur entrée pour commencer votre tour !\n");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("---                     C'est au tour de L'IA !                     ---\n");
+                Console.WriteLine("> Votre argent : {0}                       - L'Argent de l'IA : {1}\n", joueurHumain.argent, joueurMachine.argent);
+                Console.WriteLine("> SES cartes actives :");
+                foreach (Card c in joueurMachine.main)
+                {
+                    Console.WriteLine(" - " + c.ToString());
+                }
+                Console.WriteLine("\n\nAppuyer sur entrée pour commencer SON tour !\n");
+                Console.ReadLine();
+            }
         }
     }
 }
